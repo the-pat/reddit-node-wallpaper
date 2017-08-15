e3bc79b82413a06a9973cfea2dd02dab46a8b4ef
Patrick Tone <tone@thepat.xyz>
2017-08-15 09:05:18 -0500

    Added a config file and moved the main functionality to the `change` function
    TODO: finish implementing the config (filter by: score and domains) and get the correct file type from the image (rather than assuming that it is a `jpg`)

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
+const config = require('./config.json');
 const fs = require('fs');
 const request = require('request');
 const rp = require('request-promise-native');
 const wallpaper = require('wallpaper');
 
+const change = () => {
+    const url = `https://www.reddit.com/r/${random(config.subreddits)}/${config.sort}.json?t=${config.from}limit=${config.limit}`;
+    const options = {
+        uri: url,
+        json: true,
+        simple: true,
+    };
+
+    const path = './wallpaper.jpg';
+
+    rp(options)
+        .then(getURL)
+        .then(url => download(url, path))
+        .then(() => wallpaper.set(path))
+        .then(() => console.log('success!'))
+        .catch(error => console.error(error));
+};
+
 const getURL = obj => new Promise(resolve => {
-    const posts = obj.data.children.filter(post => post.data.url.length && !post.data.over_18);
-    const index = Math.floor(Math.random() * posts.length);
-    const url = posts[index].data.url;
+    const posts = obj.data.children.filter(post => post.kind.toLowerCase() === 't3' && post.data && !post.data.over_18)
+                                   .map(post => ({
+                                       url: post.data.url,
+                                       domain: post.data.domain.toLowerCase(),
+                                       //type: type(post.data.url),
+                                   }));
+    const url = random(posts).url;
 
     resolve(url);
 });
 
+const random = array => array[Math.floor(Math.random() * array.length)];
+
 const download = (uri, path) => new Promise(resolve => request(uri).pipe(fs.createWriteStream(path)).on('close', resolve));
 
-const set = (path) => wallpaper.set(path);
-
-let options = {
-    uri: 'https://www.reddit.com/r/earthporn/hot.json?limit=10',
-    json: true,
-};
-
-let path = './wallpaper.jpg';
-
-rp(options)
-    .then(getURL)
-    .then(url => download(url, path))
-    .then(() => set(path))
-    .then(() => console.log('success!'))
-    .catch(error => console.error(error));
+change();
