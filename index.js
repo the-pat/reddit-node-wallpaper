c20b0c1f77d57735168b254ccae08aab2ec9eeb2
Patrick Tone <tone@thepat.xyz>
2017-08-23 06:55:58 -0500

    added filter configuration for domains
    work on the TODOs to help improve the overall functionality of the module. it would be nice not to have to run the module over and over until it stops grabbing imgur album webpages. i'd also like to break it up into small modules (i.e. imgur images and cli).

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const rp = require('request-promise-native');
 const wallpaper = require('wallpaper');
 
 const change = () => {
-    const url = `https://www.reddit.com/r/${random(config.subreddits)}/${config.sort}.json?t=${config.from}limit=${config.limit}`;
+    const url = `https://www.reddit.com/r/${randomElement(config.subreddits)}/${config.sort}.json?t=${config.from}limit=${config.limit}`;
     const options = {
         uri: url,
         json: true,
@@ -17,25 +17,31 @@ const change = () => {
     rp(options)
         .then(getURL)
         .then(url => download(url, path))
+        // TODO: get file type here and append to 'wallpaper'..reject if an html page
         .then(() => wallpaper.set(path))
         .then(() => console.log('success!'))
         .catch(error => console.error(error));
 };
 
 const getURL = obj => new Promise(resolve => {
-    const posts = obj.data.children.filter(post => post.kind.toLowerCase() === 't3' && post.data && !post.data.over_18)
+    const posts = obj.data.children.filter(post => post.kind.toLowerCase() === 't3' &&
+                                                   post.data &&
+                                                   config.domains.includes(post.data.domain.toLowerCase()) &&
+                                                   !post.data.over_18)
                                    .map(post => ({
                                        url: post.data.url,
                                        domain: post.data.domain.toLowerCase(),
                                        //type: type(post.data.url),
                                    }));
-    const url = random(posts).url;
+    // TODO: if all posts are filtered, reject and try again
+    const url = randomElement(posts).url;
 
     resolve(url);
 });
 
-const random = array => array[Math.floor(Math.random() * array.length)];
+const randomElement = array => array[Math.floor(Math.random() * array.length)];
 
+// TODO: create/find an imgur download module (get help grabbing an image from an album)
 const download = (uri, path) => new Promise(resolve => request(uri).pipe(fs.createWriteStream(path)).on('close', resolve));
 
 change();
