113d49af247ab21844c40799c02fe6bbda775eba
Patrick Tone <pt@patrickt.one>
2017-08-24 07:10:18 -0500

    determine file type after download for a more precise file name (rather than assuming it is a jpg)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const config = require('./config.json');
+const fileType = require('file-type');
 const fs = require('fs');
 const request = require('request');
 const rp = require('request-promise-native');
@@ -12,18 +13,21 @@ const change = () => {
         simple: true,
     };
 
-    const path = './wallpaper.jpg';
+    const path = './wallpaper';
+    let name = '';
 
     rp(options)
         .then(getURL)
         .then(url => download(url, path))
-        // TODO: get file type here and append to 'wallpaper'..reject if an html page
-        .then(() => wallpaper.set(path))
+        .then(() => getType(path))
+        // TODO: reject if an html page
+        .then(type => { name = `${path}.${type.ext}`; fs.rename(path, name); })
+        .then(() => wallpaper.set(name))
         .then(() => console.log('success!'))
         .catch(error => console.error(error));
 };
 
-const getURL = obj => new Promise(resolve => {
+const getURL = obj => new Promise((resolve, reject) => {
     const posts = obj.data.children.filter(post => post.kind.toLowerCase() === 't3' &&
                                                    post.data &&
                                                    config.domains.includes(post.data.domain.toLowerCase()) &&
@@ -31,9 +35,12 @@ const getURL = obj => new Promise(resolve => {
                                    .map(post => ({
                                        url: post.data.url,
                                        domain: post.data.domain.toLowerCase(),
-                                       //type: type(post.data.url),
                                    }));
     // TODO: if all posts are filtered, reject and try again
+    if (!posts) {
+        reject('No posts match the config criteria.');
+        return;
+    }
     const url = randomElement(posts).url;
 
     resolve(url);
@@ -43,5 +50,7 @@ const randomElement = array => array[Math.floor(Math.random() * array.length)];
 
 // TODO: create/find an imgur download module (get help grabbing an image from an album)
 const download = (uri, path) => new Promise(resolve => request(uri).pipe(fs.createWriteStream(path)).on('close', resolve));
+
+const getType = path => new Promise((resolve, reject) => fs.readFile(path, (err, data) => err ? reject(err) : resolve(fileType(data))));
 
 change();
