ca5250ec7977e994c770132ad9624e166e20b20e
Anthony Parrett <anthonyparrett7@gmail.com>
2018-10-09 20:27:07 -0500

    Use async/await instead of callbacks

--- index.js
+++ index.js
@@ -5,79 +5,75 @@ const request = require('request');
 const rp = require('request-promise-native');
 const wallpaper = require('wallpaper');
 
-const change = (start = 0) => {
-    if (!config) {
-        console.log('Please configure the `config.json`.');
-        return;
-    }
+const change = async (start = 0) => {
+	try {
+		if (!config) {
+			return console.log('Please configure the `config.json`.');
+		}
 
-    if (start === config.maxRetry) {
-        console.log('We tried our best, but were unable to change the wallpaper. Sorry.');
-        return;
-    }
-  
-    const url = `https://www.reddit.com/r/${randomElement(config.subreddits)}/${config.sort}.json?t=${config.from}&limit=${config.limit}`;
-    const options = {
-        uri: url,
-        json: true,
-        simple: true,
-    };
-    
-    const path = config.directory + '/wallpaper';
-    let name = '';
+		if (start === config.maxRetry) {
+			return console.log('We tried our best, but were unable to change the wallpaper. Sorry.');
+		}
+	
+		const url = `https://www.reddit.com/r/${randomElement(config.subreddits)}/${config.sort}.json?t=${config.from}&limit=${config.limit}`;
+		const options = {
+			uri: url,
+			json: true,
+			simple: true,
+		};
+		
+		const path = config.directory + '/wallpaper';
+		let name = '';
 
-    if (!fs.existsSync(config.directory)) {
-        fs.mkdirSync(config.directory);
-    }
+		if (!fs.existsSync(config.directory)) {
+			fs.mkdirSync(config.directory);
+		}
 
-    rp(options)
-        .then(getURL)
-        .then(url => download(url, path))
-        .then(() => getType(path))
-        .then(type => new Promise((resolve, reject) => {
-            if (!type || !config.types.includes(type.ext)) {
-                reject('type is null');
-                return;
-            }
+		const obj = await rp(options);
+		const imgUrl = await getURL(obj);
+		await download(imgUrl, path);
+		const type = await getType(path);
+		
+		if (!type || !config.types.includes(type.ext)) throw 'type is null';
 
-            name = `${path}.${type.ext}`;
-            fs.rename(path, name);
-            resolve();
-        }))
-        .then(() => wallpaper.set(name))
-        .then(() => console.log('success!'))
-        .catch(error => {
-            console.error(error);
-            change(start + 1);
-        });
+		name = `${path}.${type.ext}`;
+		fs.renameSync(path, name);
+    wallpaper.set(name);
+    console.log('Success!');
+	}
+	catch (e) {
+		console.error(e);
+		change(start + 1);
+	}
 };
 
-const getURL = obj => new Promise((resolve, reject) => {
-    const posts = obj.data.children.filter(post => post.kind.toLowerCase() === 't3' &&
-        post.data &&
-        config.domains.includes(post.data.domain.toLowerCase()) &&
-        post.data.over_18 === (config.over_18 === 'true') &&
-        post.data.score >= config.score &&
-        post.data.preview.images[0].source.width >= config.minimum_image_size.width &&
-        post.data.preview.images[0].source.height >= config.minimum_image_size.height)
-        .map(post => ({
-            url: post.data.preview.images[0].source.url,
-            domain: post.data.domain.toLowerCase(),
-        }));
+const getURL = async obj => {
+	const posts = obj.data.children
+		.filter(post => 
+			post.kind.toLowerCase() === 't3' &&
+			post.data &&
+			config.domains.includes(post.data.domain.toLowerCase()) &&
+			post.data.over_18 === (config.over_18 === 'true') &&
+			post.data.score >= config.score &&
+			post.data.preview.images[0].source.width >= config.minimum_image_size.width &&
+			post.data.preview.images[0].source.height >= config.minimum_image_size.height
+		)
+		.map(post => ({
+			url: post.data.preview.images[0].source.url,
+			domain: post.data.domain.toLowerCase(),
+		}));
 
-    if (!posts) {
-        reject('No posts match the config criteria.');
-        return;
-    }
-    const url = randomElement(posts).url;
+	if (!posts) {
+		throw 'No posts match the config criteria.';
+	}
 
-    resolve(url);
-});
+	return randomElement(posts).url;
+};
 
 const randomElement = array => array[Math.floor(Math.random() * array.length)];
 
 const download = (uri, path) => new Promise(resolve => request(uri).pipe(fs.createWriteStream(path)).on('close', resolve));
 
 const getType = path => new Promise((resolve, reject) => fs.readFile(path, (err, data) => err ? reject(err) : resolve(fileType(data))));
-
+		
 change();
